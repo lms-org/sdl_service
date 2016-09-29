92e9b4aa214cc92b410d8d0f9b57d75acf1c6edd
phibedy <philipp.schmette@gmx.de>
2016-09-29 19:52:23 +0200

    conan bin export

--- conanfile.py
+++ conanfile.py
@@ -17,8 +17,7 @@ class SdlServiceConan(ConanFile):
         self.copy("*.h", dst="include",src="include")
         self.copy("*.lib", dst="lib", src="lib")
         self.copy("*.a", dst="lib", src="lib")
-        self.copy("*.so", dst="lib")
+        self.copy("*.so",dst="bin")
 
     def package_info(self):
         self.cpp_info.libs = ["lms_sdl_service"]
-        self.copy("*.so",dst="bin")
